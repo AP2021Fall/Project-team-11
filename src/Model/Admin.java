543e5698ebbd2230f19738ad960786fddf934757
sepanta <sepanta1380@gmail.com>
2021-12-15 20:49:03 +0330

    agha ali 2

--- src/Model/Admin.java
+++ src/Model/Admin.java
@@ -1,7 +1,21 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Admin extends User{
+    private static ArrayList<Team> pendingTeams = new ArrayList<>();
+
+
     public Admin(String username, String password, String emailAddress) {
         super(username, password, emailAddress);
+    }
+
+
+    public static ArrayList<Team> getPendingTeams() {
+        return pendingTeams;
+    }
+
+    public static void addTeam(Team team){
+        pendingTeams.add(team);
     }
 }
